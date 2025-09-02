c2dd0da4a66a7df556aa565067f6315505ffedeb
sumit <mazumdarsumit3@gmail.com>
2025-09-02 22:42:25 +0530

    added meta txn to support gasless transaction

--- contract/src/MintNft.sol
+++ contract/src/MintNft.sol
@@ -5,13 +5,14 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
 
 /**
  * @title MintNft
  * @author Sumit Mazumdar
  * @notice Contract to allow minting different ERC1155 collections for Proof of Attendance
  */
-contract MintNft is ERC1155, Ownable {
+contract MintNft is ERC1155, Ownable, ERC2771Context {
     using ECDSA for bytes32;
     using MessageHashUtils for bytes32;
 
@@ -20,7 +21,8 @@ contract MintNft is ERC1155, Ownable {
         string symbol;
         string eventURI;
         uint256 maxSupply;
-        uint256 totalMinted; // Fixed typo: was totalMintes
+        uint256 totalMinted;
+        bytes32 eventNonce;
     }
 
     mapping(uint256 => TokenType) public tokenTypes;
@@ -29,10 +31,9 @@ contract MintNft is ERC1155, Ownable {
     uint256 public tokenTypeCounter;
 
     event TokenTypeCreated(uint256 indexed tokenId, string name, string symbol, string eventURI, uint256 maxSupply);
-
     event NFTClaimed(uint256 indexed tokenId, address indexed claimer);
 
-    constructor() ERC1155("") Ownable(msg.sender) {}
+    constructor(address trustedForwarder) ERC1155("") Ownable(msg.sender) ERC2771Context(trustedForwarder) {}
 
     function createTokenType(string memory name, string memory symbol, string memory eventURI, uint256 maxSupply)
         external
@@ -42,12 +43,15 @@ contract MintNft is ERC1155, Ownable {
         tokenTypeCounter++;
         uint256 newTokenId = tokenTypeCounter;
 
+        bytes32 eventNonce = keccak256(abi.encodePacked(newTokenId, block.timestamp ,msg.sender));
+
         tokenTypes[newTokenId] = TokenType({
             name: name,
             symbol: symbol,
             eventURI: eventURI,
             maxSupply: maxSupply,
-            totalMinted: 0 // Fixed to match struct field
+            totalMinted: 0, // Fixed to match struct field
+            eventNonce: eventNonce
         });
 
         emit TokenTypeCreated(newTokenId, name, symbol, eventURI, maxSupply);
@@ -62,24 +66,44 @@ contract MintNft is ERC1155, Ownable {
         tokenTypes[tokenId].totalMinted += amount;
     }
 
-    function claimFor(uint256 tokenId, address claimer, bytes memory signature, bytes32 nonce) external onlyOwner {
+    function claim(uint256 tokenId, bytes memory signature) external {
+        address claimer = _msgSender();
+
         require(tokenTypes[tokenId].maxSupply > 0, "Token type does not exist");
+
         require(!hasClaimed[tokenId][claimer], "Already claimed this token type");
-        require(!usedNonces[nonce], "Nonce already used");
+        bytes32 eventNonce = tokenTypes[tokenId].eventNonce;
+        require(eventNonce != 0, "Invalid event nonce");
+
+        bytes32 claimNonce = keccak256(abi.encodePacked(tokenId, claimer, block.timestamp));
+        require(!usedNonces[claimNonce], "Nonce already used");
         require(tokenTypes[tokenId].totalMinted < tokenTypes[tokenId].maxSupply, "All NFTs of this type claimed");
 
-        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, claimer, nonce));
+        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, claimer, eventNonce ,claimNonce));
         bytes32 ethSignedHash = messageHash.toEthSignedMessageHash();
         address signer = ethSignedHash.recover(signature);
 
         require(signer == claimer, "Invalid signature");
 
         hasClaimed[tokenId][claimer] = true;
-        usedNonces[nonce] = true;
+        usedNonces[claimNonce] = true;
         _mint(claimer, tokenId, 1, "");
         tokenTypes[tokenId].totalMinted++;
 
         emit NFTClaimed(tokenId, claimer);
+    }
+
+     // Override context functions for meta-transactions
+    function _msgSender() internal view override(ERC2771Context, Context) returns (address sender) {
+        return ERC2771Context._msgSender();
+    }
+
+    function _msgData() internal view override(ERC2771Context, Context) returns (bytes calldata) {
+        return ERC2771Context._msgData();
+    }
+
+    function _contextSuffixLength() internal view override(ERC2771Context, Context) returns (uint256) {
+        return ERC2771Context._contextSuffixLength();
     }
 
     function uri(uint256 tokenId) public view override returns (string memory) {
@@ -99,11 +123,12 @@ contract MintNft is ERC1155, Ownable {
             string memory symbol,
             string memory eventURI,
             uint256 maxSupply,
-            uint256 totalMinted
+            uint256 totalMinted,
+            bytes32 eventNonce
         )
     {
         TokenType storage token = tokenTypes[tokenId];
-        return (token.name, token.symbol, token.eventURI, token.maxSupply, token.totalMinted);
+        return (token.name, token.symbol, token.eventURI, token.maxSupply, token.totalMinted, token.eventNonce);
     }
 
     function getTotalEvents() public view returns (uint256) {
--- contract/test/MintNftTest.test.sol
+++ contract/test/MintNftTest.test.sol
@@ -1,264 +1,335 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
-
 import {Test} from "forge-std/Test.sol";
 import {MintNft} from "src/MintNft.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ERC2771Forwarder} from "@openzeppelin/contracts/metatx/ERC2771Forwarder.sol";
 
 contract MintNftTest is Test {
     using ECDSA for bytes32;
     using MessageHashUtils for bytes32;
-
     MintNft public nft;
+    ERC2771Forwarder public forwarder;
     address public owner = address(1);
     address public user1;
     address public user2;
-
     uint256 user1PrivateKey = 0x2;
     uint256 user2PrivateKey = 0x3;
-
+    
     function setUp() public {
         user1 = vm.addr(user1PrivateKey);
         user2 = vm.addr(user2PrivateKey);
-
+        
+        // Deploy the forwarder contract
+        forwarder = new ERC2771Forwarder("ERC2771Forwarder");
+        
         vm.prank(owner);
-        nft = new MintNft();
-
+        nft = new MintNft(address(forwarder));
         vm.deal(owner, 10 ether);
         vm.deal(user1, 1 ether);
     }
-
+    
     function testOwner() public view {
         assertEq(nft.owner(), owner);
     }
-
+    
     function testCreateTokenType() public {
         vm.prank(owner);
         uint256 tokenId = nft.createTokenType("Test Event", "test", "ipfs://QmTest", 100);
-
         assertEq(tokenId, 1);
     }
-
+    
     function testGetTokenType() public {
-        vm.prank(owner);
+        vm.startPrank(owner);
+        uint256 fixedTimestamp = 160000;
+        vm.warp(fixedTimestamp);
         uint256 tokenId = nft.createTokenType("Test Event", "test", "ipfs://QmTest", 100);
-
-        (string memory name, string memory symbol, string memory eventURI, uint256 maxSupply, uint256 totalMinted) =
+        bytes32 eventNonceTest = keccak256(abi.encodePacked(tokenId, fixedTimestamp, owner));
+        vm.stopPrank();
+        
+        (string memory name, string memory symbol, string memory eventURI, uint256 maxSupply, uint256 totalMinted, bytes32 eventNonce) =
             nft.getTokenType(tokenId);
-
         assertEq(name, "Test Event");
         assertEq(symbol, "test");
         assertEq(eventURI, "ipfs://QmTest");
         assertEq(maxSupply, 100);
         assertEq(totalMinted, 0);
+        assertEq(eventNonce, eventNonceTest);
     }
-
+    
     function testClaimForPublic() public {
         vm.prank(owner);
         uint256 tokenId = nft.createTokenType("Test Event", "test", "ipfs://QmTest", 100);
-
         vm.prank(owner);
         nft.mint(tokenId, 10);
-
-        //
-        bytes32 nonce = keccak256(abi.encodePacked("unique_nonce"));
-        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, user1, nonce));
+        (,,,,,bytes32 eventNonce) = nft.getTokenType(tokenId);
+        
+        // Set a fixed timestamp to ensure predictable nonce generation
+        uint256 fixedTimestamp = 160000;
+        vm.warp(fixedTimestamp);
+        
+        // Generate the same claimNonce the contract will generate
+        bytes32 claimNonce = keccak256(abi.encodePacked(tokenId, user1, fixedTimestamp));
+    
+        // Create the message hash with the claimNonce
+        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, user1, eventNonce, claimNonce));
         bytes32 ethSignedMessageHash = messageHash.toEthSignedMessageHash();
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(user1PrivateKey, ethSignedMessageHash);
         bytes memory signature = abi.encodePacked(r, s, v);
-
-        vm.prank(owner);
-        nft.claimFor(tokenId, user1, signature, nonce);
-
+    
+        vm.prank(user1);
+        nft.claim(tokenId, signature);
         assertEq(nft.balanceOf(user1, tokenId), 1);
-        assertTrue(nft.hasClaimed(tokenId, user1));
-        assertTrue(nft.usedNonces(nonce));
-
-        (,,,, uint256 totalMinted) = nft.getTokenType(tokenId);
+        assertTrue(nft.hasUserClaimed(tokenId, user1));
+        assertTrue(nft.usedNonces(claimNonce));
+        (,,,, uint256 totalMinted,) = nft.getTokenType(tokenId);
         assertEq(totalMinted, 11);
     }
-
+    
     function testCheckClaimIsGasless() public {
         vm.prank(owner);
         uint256 tokenId = nft.createTokenType("Test Event", "test", "ipfs://QmTest", 100);
-
         vm.prank(owner);
         nft.mint(tokenId, 10);
-
-        uint256 gasPrice = 20 gwei;
-        vm.txGasPrice(gasPrice);
-
-        uint256 ownerBalanceBefore = owner.balance;
-        uint256 userBalanceBefore = user1.balance;
-
-        bytes32 nonce = keccak256(abi.encodePacked("unique_nonce"));
-        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, user1, nonce));
+        (,,,,,bytes32 eventNonce) = nft.getTokenType(tokenId);
+        
+        // Set a fixed timestamp to ensure predictable nonce generation
+        uint256 fixedTimestamp = 160000;
+        vm.warp(fixedTimestamp);
+        
+        // Generate the same claimNonce the contract will generate
+        bytes32 claimNonce = keccak256(abi.encodePacked(tokenId, user1, fixedTimestamp));
+        
+        // Create the message hash with the claimNonce
+        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, user1, eventNonce, claimNonce));
         bytes32 ethSignedMessageHash = messageHash.toEthSignedMessageHash();
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(user1PrivateKey, ethSignedMessageHash);
         bytes memory signature = abi.encodePacked(r, s, v);
-
-        uint256 gasBefore = gasleft();
-
-        vm.prank(owner);
-        nft.claimFor(tokenId, user1, signature, nonce);
-
-        uint256 gasUsed = gasBefore - gasleft();
-        uint256 gasCost = gasUsed * gasPrice;
-
-        // Manually deduct gas cost from owner's balance
-        vm.deal(owner, owner.balance - gasCost);
-
-        assertEq(nft.balanceOf(user1, tokenId), 1);
-        assertTrue(nft.hasClaimed(tokenId, user1));
-        assertTrue(nft.usedNonces(nonce));
-
-        (,,,, uint256 totalMinted) = nft.getTokenType(tokenId);
-        assertEq(totalMinted, 11);
-
-        uint256 ownerBalanceAfter = owner.balance;
-        uint256 userBalanceAfter = user1.balance;
-
-        assertEq(userBalanceBefore, userBalanceAfter);
-        assertEq(ownerBalanceBefore - ownerBalanceAfter, gasCost);
-
-        emit log_named_uint("Gas used", gasUsed);
-        emit log_named_uint("Gas price", gasPrice);
-        emit log_named_uint("Gas cost", gasCost);
+        
+        // Create the function call data (only tokenId and signature)
+        bytes memory data = abi.encodeWithSelector(
+            nft.claim.selector,
+            tokenId,
+            signature
+        );
+        
+        // Create the forward request
+        ERC2771Forwarder.ForwardRequestData memory request = ERC2771Forwarder.ForwardRequestData({
+            from: user1,
+            to: address(nft),
+            value: 0,
+            gas: 500000,
+            deadline: uint48(block.timestamp + 3600),
+            data: data,
+            signature: "" // We will set it after signing
+        });
+        
+        // Get the current nonce for the user
+        uint256 nonce = forwarder.nonces(user1);
+        
+        // Build the EIP-712 hash
+        bytes32 structHash = keccak256(
+            abi.encode(
+                keccak256("ForwardRequest(address from,address to,uint256 value,uint256 gas,uint256 nonce,uint48 deadline,bytes data)"),
+                request.from,
+                request.to,
+                request.value,
+                request.gas,
+                nonce,
+                request.deadline,
+                keccak256(request.data)
+            )
+        );
+        
+        // Get the domain separator (manually compute if not exposed)
+        bytes32 domainSeparator = keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256(bytes("ERC2771Forwarder")),
+                keccak256(bytes("1")),
+                block.chainid,
+                address(forwarder)
+            )
+        );
+        
+        // Build the digest
+        bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
+        
+        // Sign the digest
+        (uint8 vReq, bytes32 rReq, bytes32 sReq) = vm.sign(user1PrivateKey, digest);
+        bytes memory requestSignature = abi.encodePacked(rReq, sReq, vReq);
+        
+        // Set the signature in the request
+        request.signature = requestSignature;
+        
+        // Verify the request is valid
+        assertTrue(forwarder.verify(request));
+        
+        // Execute the meta-transaction via the forwarder
+        forwarder.execute(request);
+        
+        // Verify the claim was successful
+        assertTrue(nft.hasUserClaimed(tokenId, user1));
     }
-
+    
     function testUserCannotDoubleClaim() public {
         vm.prank(owner);
         uint256 tokenId = nft.createTokenType("Test Event", "test", "ipfs://QmTest", 100);
-
         vm.prank(owner);
         nft.mint(tokenId, 10);
-
-        //
-        bytes32 nonce = keccak256(abi.encodePacked("unique_nonce"));
-        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, user1, nonce));
+        
+        // First claim
+        uint256 fixedTimestamp = 160000;
+        vm.warp(fixedTimestamp);
+        
+        bytes32 claimNonce = keccak256(abi.encodePacked(tokenId, user1, fixedTimestamp));
+        (,,,,,bytes32 eventNonce) = nft.getTokenType(tokenId);
+        
+        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, user1, eventNonce, claimNonce));
         bytes32 ethSignedMessageHash = messageHash.toEthSignedMessageHash();
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(user1PrivateKey, ethSignedMessageHash);
         bytes memory signature = abi.encodePacked(r, s, v);
-
-        vm.prank(owner);
-        nft.claimFor(tokenId, user1, signature, nonce);
+        
+        vm.prank(user1);
+        nft.claim(tokenId, signature);
+        
+        // Try to claim again with same parameters
         vm.expectRevert("Already claimed this token type");
-        vm.prank(owner);
-        nft.claimFor(tokenId, user1, signature, nonce);
+        vm.prank(user1);
+        nft.claim(tokenId, signature);
     }
-
+    
     function testGetRemainingSupply() public {
         vm.prank(owner);
         uint256 tokenId = nft.createTokenType("Test Event", "test", "ipfs://QmTest", 100);
-
         vm.prank(owner);
         nft.mint(tokenId, 10);
-
-        //
-        bytes32 nonce = keccak256(abi.encodePacked("unique_nonce"));
-        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, user1, nonce));
+        
+        // Claim one NFT
+        uint256 fixedTimestamp = 160000;
+        vm.warp(fixedTimestamp);
+        
+        bytes32 claimNonce = keccak256(abi.encodePacked(tokenId, user1, fixedTimestamp));
+        (,,,,,bytes32 eventNonce) = nft.getTokenType(tokenId);
+        
+        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, user1, eventNonce, claimNonce));
         bytes32 ethSignedMessageHash = messageHash.toEthSignedMessageHash();
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(user1PrivateKey, ethSignedMessageHash);
         bytes memory signature = abi.encodePacked(r, s, v);
-
-        vm.prank(owner);
-        nft.claimFor(tokenId, user1, signature, nonce);
-
-        (,,, uint256 maxSupply, uint256 totalMinted) = nft.getTokenType(tokenId);
+        
+        vm.prank(user1);
+        nft.claim(tokenId, signature);
+        
+        (,,, uint256 maxSupply, uint256 totalMinted,) = nft.getTokenType(tokenId);
         assertEq(maxSupply - totalMinted, nft.remainingSupply(tokenId));
     }
-
+    
     function test_RevertWhenWrongSignature() public {
         vm.prank(owner);
         uint256 tokenId = nft.createTokenType("Test Event", "test", "ipfs://QmTest", 100);
-
         vm.prank(owner);
         nft.mint(tokenId, 10);
-
-        //
-        bytes32 nonce = keccak256(abi.encodePacked("unique_nonce"));
-        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, user1, nonce));
+        
+        uint256 fixedTimestamp = 160000;
+        vm.warp(fixedTimestamp);
+        
+        bytes32 claimNonce = keccak256(abi.encodePacked(tokenId, user1, fixedTimestamp));
+        (,,,,,bytes32 eventNonce) = nft.getTokenType(tokenId);
+        
+        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, user1, eventNonce, claimNonce));
         bytes32 ethSignedMessageHash = messageHash.toEthSignedMessageHash();
+        
+        // Sign with user2's private key instead of user1
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(user2PrivateKey, ethSignedMessageHash);
         bytes memory signature = abi.encodePacked(r, s, v);
-
+        
         vm.expectRevert("Invalid signature");
-        vm.prank(owner);
-        nft.claimFor(tokenId, user1, signature, nonce);
+        vm.prank(user1);
+        nft.claim(tokenId, signature);
     }
-
+    
     function testSetURI() public {
         vm.prank(owner);
         uint256 tokenId = nft.createTokenType("Test Event", "test", "ipfs://QmTest", 100);
-
         string memory returnedUri = nft.uri(tokenId);
         assertEq(returnedUri, "ipfs://QmTest");
     }
-
+    
     function testGetTotalEvents() public {
         vm.prank(owner);
         nft.createTokenType("Test Event 1", "test", "ipfs://QmTest", 100);
-
         vm.prank(owner);
         nft.createTokenType("Test Event 2", "test", "ipfs://QmTest", 100);
-
         assertEq(nft.getTotalEvents(), 2);
     }
-
+    
     function testUserHasClaimed() public {
         vm.prank(owner);
         uint256 tokenId = nft.createTokenType("Test Event 1", "test", "ipfs://QmTest", 100);
-
-        bytes32 nonce = keccak256(abi.encodePacked("unique_nonce"));
-        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, user1, nonce));
+        
+        uint256 fixedTimestamp = 160000;
+        vm.warp(fixedTimestamp);
+        
+        bytes32 claimNonce = keccak256(abi.encodePacked(tokenId, user1, fixedTimestamp));
+        (,,,,,bytes32 eventNonce) = nft.getTokenType(tokenId);
+        
+        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, user1, eventNonce, claimNonce));
         bytes32 ethSignedMessageHash = messageHash.toEthSignedMessageHash();
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(user1PrivateKey, ethSignedMessageHash);
         bytes memory signature = abi.encodePacked(r, s, v);
-
-        vm.prank(owner);
-        nft.claimFor(tokenId, user1, signature, nonce);
-
+        
+        vm.prank(user1);
+        nft.claim(tokenId, signature);
+        
         assertTrue(nft.hasUserClaimed(tokenId, user1));
     }
-
+    
     function testGetTokenStats() public {
         vm.prank(owner);
         uint256 tokenId = nft.createTokenType("Test Event 1", "test", "ipfs://QmTest", 100);
-
         vm.prank(owner);
         nft.mint(tokenId, 50);
-
-        bytes32 nonce = keccak256(abi.encodePacked("unique_nonce"));
-        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, user1, nonce));
+        
+        uint256 fixedTimestamp = 160000;
+        vm.warp(fixedTimestamp);
+        
+        bytes32 claimNonce = keccak256(abi.encodePacked(tokenId, user1, fixedTimestamp));
+        (,,,,,bytes32 eventNonce) = nft.getTokenType(tokenId);
+        
+        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, user1, eventNonce, claimNonce));
         bytes32 ethSignedMessageHash = messageHash.toEthSignedMessageHash();
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(user1PrivateKey, ethSignedMessageHash);
         bytes memory signature = abi.encodePacked(r, s, v);
-
-        vm.prank(owner);
-        nft.claimFor(tokenId, user1, signature, nonce);
-
+        
+        vm.prank(user1);
+        nft.claim(tokenId, signature);
+        
         (uint256 maxSupply, uint256 totalMinted, uint256 remaining) = nft.getEventStats(tokenId);
         assertEq(maxSupply, 100);
         assertEq(totalMinted, 51);
         assertEq(remaining, maxSupply - totalMinted);
     }
-
+    
     function test__RevertWhenClaimAmountExceeds() public {
         vm.prank(owner);
         uint256 tokenId = nft.createTokenType("Test Event 1", "test", "ipfs://QmTest", 100);
-
         vm.prank(owner);
         nft.mint(tokenId, 100);
-
-        bytes32 nonce = keccak256(abi.encodePacked("unique_nonce"));
-        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, user1, nonce));
+        
+        uint256 fixedTimestamp = 160000;
+        vm.warp(fixedTimestamp);
+        
+        bytes32 claimNonce = keccak256(abi.encodePacked(tokenId, user1, fixedTimestamp));
+        (,,,,,bytes32 eventNonce) = nft.getTokenType(tokenId);
+        
+        bytes32 messageHash = keccak256(abi.encodePacked(tokenId, user1, eventNonce, claimNonce));
         bytes32 ethSignedMessageHash = messageHash.toEthSignedMessageHash();
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(user1PrivateKey, ethSignedMessageHash);
         bytes memory signature = abi.encodePacked(r, s, v);
-
+        
         vm.expectRevert("All NFTs of this type claimed");
-        vm.prank(owner);
-        nft.claimFor(tokenId, user1, signature, nonce);
+        vm.prank(user1);
+        nft.claim(tokenId, signature);
     }
 }
